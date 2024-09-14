f9b5230c418890f7cade84e789340b7af6e5f33d
lucyferrabee <lucy.ferrabee@hotmail.com>
2024-09-14 17:04:09 +0100

    change to using DBAL instead of ORM and add services and models

--- src/Command/ProcessBlobReferencesCommand.php
+++ src/Command/ProcessBlobReferencesCommand.php
@@ -2,36 +2,48 @@
 
 namespace App\Command;
 
-use App\Service\BlobReferenceService;
+use App\Service\BlobConsistencyService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class ProcessBlobReferencesCommand extends Command
 {
-    protected static $defaultName = 'app:check-blob-references';
-    
-    private $blobReferenceService;
+    protected static $defaultName = 'app:check-blobs';
 
-    public function __construct(BlobReferenceService $blobReferenceService)
+    private BlobConsistencyService $blobReferenceService;
+
+    public function __construct(BlobConsistencyService $blobReferenceService)
     {
-        $this->blobReferenceService = $blobReferenceService;
         parent::__construct();
+        $this->blobReferenceService = $blobReferenceService;
     }
 
     protected function configure()
     {
         $this
-            ->setDescription('Checks the consistency of blob references.');
+            ->setDescription('Checks for blob inconsistencies across specified tables.')
+            ->addArgument('table', InputArgument::OPTIONAL, 'Table to check (or leave empty to check all tables)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $table = $input->getArgument('table');
 
-        // Run the blob consistency check
-        $this->blobReferenceService->checkBlobConsistency($io);
+        try {
+            if ($table) {
+                $this->blobReferenceService->processReferencesForTable($table, $io);
+            } else {
+                $this->blobReferenceService->processAllReferences($io);
+            }
+            $io->success('Blob check completed successfully.');
+        } catch (\Exception $e) {
+            $io->error('Error during blob check: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
--- src/Kernel.php
+++ src/Kernel.php
@@ -3,22 +3,8 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new class implements \Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface {
-            public function process(ContainerBuilder $container)
-            {
-                foreach ($container->findTaggedServiceIds('console.command') as $id => $tags) {
-                    echo "Found command: $id\n";
-                }
-            }
-        }, PassConfig::TYPE_BEFORE_OPTIMIZATION, -1000);
-    }
 }
